791cefad4b7ee2c18e6865690ba86641225671d3
Mihir Vilas Marathe <46415914+Mihir1030@users.noreply.github.com>
2022-04-19 11:04:55 +0530

    2c2p

--- src/main/java/Settle2.java
+++ src/main/java/Settle2.java
@@ -16,10 +16,12 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.shaded.json.JSONObject;
 import com.nimbusds.jwt.EncryptedJWT;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.Charset;
@@ -55,11 +57,33 @@ public class Settle2 {
         FileInputStream is = new FileInputStream(
             "/Users/mihirvmarathe/IdeaProjects/2c2p/2c2p/jwt.cer"); ////2c2p public cert key
 
+
         JWEAlgorithm alg = JWEAlgorithm.RSA_OAEP;
         EncryptionMethod enc = EncryptionMethod.A256GCM;
 
+        String pub = "-----BEGIN CERTIFICATE-----\n"
+            + "MIIDLjCCAhYCCQCYGrdwIYmDtzANBgkqhkiG9w0BAQsFADBZMQswCQYDVQQGEwJT\n"
+            + "RzELMAkGA1UECAwCU0cxCzAJBgNVBAcMAlNHMRIwEAYDVQQKDAkyQzJQIERlbW8x\n"
+            + "DTALBgNVBAsMBDJjMnAxDTALBgNVBAMMBDJjMnAwHhcNMjAxMDI5MDYyNDAzWhcN\n"
+            + "MzAxMDI3MDYyNDAzWjBZMQswCQYDVQQGEwJTRzELMAkGA1UECAwCU0cxCzAJBgNV\n"
+            + "BAcMAlNHMRIwEAYDVQQKDAkyQzJQIERlbW8xDTALBgNVBAsMBDJjMnAxDTALBgNV\n"
+            + "BAMMBDJjMnAwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDJuLHNVQSV\n"
+            + "L6eZUDR+5D9z0t/gd8fiLKcoNDmPfFS3d1p1KH1VcttTM4KxqC0/jiDs3BqBjUuO\n"
+            + "6QQB+vbmV3dQZ/RO3iKgfE3fALBCiDjU6zVp9ZbWQHubyHPLMuHCBS+8EFKgBqCw\n"
+            + "I1CTE5x26tskibJYOsExeYornSwGEJkXnXodDsca6sgkcnm8jVNyOYL5HG3KtuFp\n"
+            + "AqU9bCfn7QLdCWbJa19exaq1o32UYPSla1Rm15xoByVlP7CxRnReSXCZDF54dUq/\n"
+            + "6hw3Jdf1+rIn3rJqkHWXPdX0HMFklieeVXN/GM+8xbkvp9GEXDpvOuO/jgCKZ03z\n"
+            + "TKXXpaAumHPVAgMBAAEwDQYJKoZIhvcNAQELBQADggEBAMUh53pskCrDgHVUwgQd\n"
+            + "SRr25+o0XPJWW+LmpW68PwFLIrQhgQqys3RHZLPeTpCBQPCd6dClhR41kp7l/uFO\n"
+            + "UjhvNcrVhqsEUEClFRu5Fos7votayJgKnvlXnJ+cI3a9cp4Z0W0tLMKus13cb6+h\n"
+            + "4kXJ/wCy0IfvUlEXtFOQM+ftjgfbhIopoxvzEzvEulYOhGI/1HKXJ5nRdJRT2unV\n"
+            + "oui9OKP1sUHiGqo73EEg5JZeRen/DJvaN8uhwayhyWSC5+NDGK6UKPFIpWKdg+rs\n"
+            + "tWK6Vty9jVevX3Y4aNrxiD/IgPYtljGbefEKRAeqEkxc+lWBF70JjpyAobG1Oxaj\n"
+            + "jPM=\n"
+            + "-----END CERTIFICATE-----";
+        InputStream targetStream = new ByteArrayInputStream(pub.getBytes());
         CertificateFactory certFactory = CertificateFactory.getInstance("X509");
-        X509Certificate jwePubKey = (X509Certificate) certFactory.generateCertificate(is);
+        X509Certificate jwePubKey = (X509Certificate) certFactory.generateCertificate(targetStream);
 
         RSAKey rsaJWE = RSAKey.parse(jwePubKey);
         RSAPublicKey jweRsaPubKey = rsaJWE.toRSAPublicKey();
@@ -130,7 +154,7 @@ public class Settle2 {
             boolean verified = jwsObjectRes.verify(new RSASSAVerifier(jweRsaPubKeyRes));
 
             if(verified) {
-                JWEObject jweRes = EncryptedJWT.parse(jwsObject.getPayload().toString());
+                JWEObject jweRes = EncryptedJWT.parse(jwsObjectRes.getPayload().toString());
                 jweRes.decrypt(new RSADecrypter(jwsPrivateKey));
                 String responsePayload = jweRes.getPayload().toString();
                 System.out.println(responsePayload);
